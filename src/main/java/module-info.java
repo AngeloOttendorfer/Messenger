7ff1c86d152c1b5193413fcca4137af866a108be
Angelo Ottendorfer <ic21b113@technikum-wien.at>
2022-12-23 17:23:40 +0100

    updated project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
-module com.example.messenger {
+module com.example.messengerchat {
     requires javafx.controls;
     requires javafx.fxml;
 
 
     opens com.example.messenger to javafx.fxml;
     exports com.example.messenger;
+    exports controller;
+    opens controller to javafx.fxml;
 }
