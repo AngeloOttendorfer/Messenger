b26eaecc16f3e29b12469d010d4c345a885c1376
Angelo Ottendorfer <ic21b113@technikum-wien.at>
2022-12-23 17:18:27 +0100

    completed LoginController.java and loginScreen.fxml

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import com.example.messenger.MessengerApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import networking.Client;
+import networking.Server;
+
+import java.io.IOException;
+
+public class LoginController {
+    @FXML
+    private TextField tf_username;
+    @FXML
+    private PasswordField pf_pwd;
+    @FXML
+    private Button btn_login;
+    @FXML
+    private Button btn_return;
+
+    public void login() throws IOException {
+        String username = tf_username.getText();
+        String password = pf_pwd.getText();
+        FXMLLoader chatLoader = new FXMLLoader(MessengerApplication.class.getResource("chat.fxml"));
+        Stage chat = (Stage) btn_login.getScene().getWindow();
+        Server server = new Server();
+        if(!server.containsUserData(username,password)){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("An Error occurred");
+            alert.setContentText("Username and/or password is incorrect");
+
+            alert.showAndWait();
+        }
+        else{
+            Client client = new Client(username, password);
+            //client.start();
+            client.connectToServer("127.0.0.1", 8000);
+            client.start();
+            System.out.println("Client is connected");
+            chat.setScene(new Scene(chatLoader.load()));
+        }
+
+    }
+
+    public void goBack()throws IOException{
+        FXMLLoader welcomeLoader = new FXMLLoader(MessengerApplication.class.getResource("welcomeScreen.fxml"));
+        Stage welcome = (Stage) btn_return.getScene().getWindow();
+        welcome.setScene(new Scene(welcomeLoader.load()));
+    }
+}
